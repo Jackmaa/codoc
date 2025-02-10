bfcb229000878116ec004b131b59c1581f170dbd
NeferNedjer <vincejurien@gmail.com>
2025-02-10 10:11:41 +0100

    createpost.php create we can now create new post !

--- controller/ControllerPost.php
+++ controller/ControllerPost.php
@@ -2,6 +2,7 @@
 
 class ControllerPost {
     public function home() {
+
         global $router;
         $model = new ModelPost();
         $datas = $model->drawPage();
@@ -16,5 +17,26 @@ class ControllerPost {
         $data  = $model->read($id);
         //var_dump($data);
         require_once './view/postpage.php';
+    }
+
+    public function create(){
+
+        global $router;
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            if(!empty($_POST['title']) && !empty($_POST['content']) && !empty($_POST['description']) && !empty($_POST['id_user'])){
+                $model = new ModelPost();
+                $model->createPost($_POST['title'], $_POST['content'], $_POST['description'], $_POST['id_user']);
+                echo "Nouceau codoc enregistré avec succès !";
+                header('Location: /codoc');
+            }else{
+                var_dump($_POST);
+                $error = "Veuillez remplir tous les champs !";
+                require_once('./view/createpost.php');
+            }
+           
+        }else{
+            require_once('./view/createpost.php');
+        }
+        
     }
 }
---
+++ view/createpost.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/assets/css/styles.css">
+    <link rel="icon" type="image/x-icon" href="../public/assets/img/light.svg">
+    <title>Create | codoc</title>
+</head>
+<body>
+    <?php if(isset($error)): ?>
+        <p><?=$error; ?></p>
+        <?php endif ?>
+
+    <form action="/codoc/post/create" method="POST">
+        <input type="hidden" name="id_user" value="<?=$_SESSION['id_user'] ?>">
+        <div>
+            <label for="title">Titre</label>
+            <input type="text" name="title" id="title" required>
+        </div>
+        <div>
+            <label for="content">Content</label>
+            <textarea name="content" id="content" required></textarea>
+        </div>
+        <div>
+            <label for="description">Description</label>
+            <input type="text" name="description" id="description" required>
+        </div>
+        <button type="submit" name="submit" value="codoc" class="btn">codoc</button>
+    </form>
+    <a href="<?= $router->generate('home')?>">Retour</a>
+</body>
+</html>
