a6f500706e44ecceede9204e7376a39b016494a1
Jackmaa <valentingillot@gmail.com>
2025-02-12 11:06:54 +0100

    :art: add favicon.svg and update icon references in multiple views :art:

--- view/dashboard.php
+++ view/dashboard.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./public/assets/css/styles.css">
+    <link rel="icon" type="image/x-icon" href="./public/assets/img/favicon.svg">
     <title>Dashboard</title>
 </head>
 <body>
